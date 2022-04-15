39be3d585e8dfdd01d5628d0f6b974bb000ce76b
RyotaKoikeda <tribestaff0706@gmail.com>
2022-04-15 13:09:13 +0900

    style fix

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import { AttendEdit, AttendMenu, Home, SignUp, StaffEdit } from "./templates";
+import { AttendEdit, Home, SignUp, StaffEdit } from "./templates";
 import Auth from "./Auth";
 
 const Router = () => {
@@ -9,7 +9,6 @@ const Router = () => {
       <Route exact path="(/)?" component={Home} />
       <Auth>
         <Route exact path="/attend/edit" component={AttendEdit} />
-        <Route exact path="/attend" component={AttendMenu} />
         <Route path="/staff/edit(/:id)?" component={StaffEdit} />
         <Route exact path="/signup" component={SignUp} />
       </Auth>
